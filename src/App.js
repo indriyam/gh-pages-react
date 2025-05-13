2e4988a685c5e6b3b04e39d6b93f83d8a19f2151
Toji Varghese <tojivarghese@rediffmail.com>
2025-05-13 11:20:38 +0530

    Updated HomePage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My Awesome GitHub Page running using React</p>
         <a
           className="App-link"
           href="https://reactjs.org"
